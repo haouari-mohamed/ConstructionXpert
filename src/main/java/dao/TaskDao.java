11faa51d5694766684c740ba001f6ba860d1c676
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-05-23 12:14:46 +0100

    task dao

---
+++ src/main/java/dao/TaskDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import model.Task;
+
+import java.util.List;
+
+public interface TaskDao {
+    void addTask(Task task);
+    void updateTask(Task task);
+    void deleteTask(Task task);
+    List<Task> getTasks();
+    Task getTask(int id);
+
+    List<Task> getTasksByProjectId(int projectId);
+}
+
